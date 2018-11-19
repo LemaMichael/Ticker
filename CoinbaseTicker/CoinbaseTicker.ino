f46e65df7940f14341725a26a5539b058ec8afca
Michael Lema <mel526@nyu.edu>
2018-11-19 03:18:30 -0500

    Crypto prices display quicker!

--- CoinbaseTicker/CoinbaseTicker.ino
+++ CoinbaseTicker/CoinbaseTicker.ino
@@ -1,40 +1,25 @@
+
+// http://api.pro.coinbase.com/products/BTC-USD/ticker
+// Here is the expected response: 
+// 
+// {
+// "trade_id":52342926,
+// "price":"6201.54000000",
+// "size":"0.00216765",
+// "bid":"6201.53",
+// "ask":"6201.54",
+// "volume":"5318.00903573",
+// "time":"2018-10-13T00:32:56.268000Z"
+// }
+ 
+
 #include "ESP8266WiFi.h"
 #include "WiFiClientSecure.h"
 #include "ArduinoJson.h"
 #include <MD_MAX72xx.h>
-#include <stdio.h>
 #include <SPI.h>
+#include <stdio.h>
 
-/*
- http://api.pro.coinbase.com/products/BTC-USD/ticker
- Expected http response
- 
- {
- "trade_id":52342926,
- "price":"6201.54000000",
- "size":"0.00216765",
- "bid":"6201.53",
- "ask":"6201.54",
- "volume":"5318.00903573",
- "time":"2018-10-13T00:32:56.268000Z"
- }
- 
- */
-
-// Turn on debug statements to the serial output
-#define  DEBUG  1
-
-#if  DEBUG
-#define PRINT(s, x) { Serial.print(F(s)); Serial.print(x); }
-#define PRINTS(x) Serial.print(F(x))
-#define PRINTD(x) Serial.println(x, DEC)
-
-#else
-#define PRINT(s, x)
-#define PRINTS(x)
-#define PRINTD(x)
-
-#endif
 
 
 // Define the number of devices we have in the chain and the hardware interface
@@ -58,15 +43,12 @@ const char* ssid = "";
 const char* password = "";
 
 const char* host = "api.pro.coinbase.com";
-const int httpsPort = 443;
 
 
 void scrollText(char *p)
 {
   uint8_t charWidth;
   uint8_t cBuf[8];  // this should be ok for all built-in fonts
-
-  PRINTS("\nScrolling text");
   mx.clear();
 
   while (*p != '\0')
@@ -98,51 +80,60 @@ void connectToWIFI() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
     
-
 }
 
 JsonObject& getObject(String url) {
 
-  // Use WiFiClientSecure class to create TLS connection
+    // Connect to HTTP server 
     WiFiClientSecure client;
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-    if (!client.connect(host, httpsPort)) {
+    client.setTimeout(10000);
+    if (!client.connect(host, 443)) {
         scrollText("Connection failed!");
         Serial.println("connection failed");
     }
+    Serial.println(F("Connected!"));
 
-    
-    Serial.print("requesting URL: ");
-    Serial.println(url);
-
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "User-Agent: BuildFailureDetectorESP8266\r\n" +
-                 "Connection: close\r\n\r\n");
-    
-    Serial.println("request sent");
-    
-    while (client.connected()) {
-        String line = client.readStringUntil('\n');
-        if (line == "\r") {
-            Serial.println("headers received");
-            break;
-        }
+    // Send HTTP Request
+    client.println((String("GET ") + url + " HTTP/1.1"));
+    client.println("Host: api.pro.coinbase.com");
+    client.println("User-Agent: BuildFailureDetectorESP8266");
+    client.println("Connection: close");
+    if (client.println() == 0) {
+      Serial.println(F("Failed to send request"));
+      scrollText("Failed to send request");
     }
-    String line = client.readStringUntil('\n');
+
+    // Check HTTP Status
+    char status[32] = {0};
+    client.readBytesUntil('\r', status, sizeof(status));
+    if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+      Serial.print(F("Unexpected response: "));
+      Serial.println(status);
+      scrollText(status);
+    }
+
+    // Skip HTTP headers
+    char endOfHeaders[] = "\r\n\r\n";
+    if (!client.find(endOfHeaders)) {
+      Serial.println(F("Invalid response"));
+      scrollText("Invalid Response");
+    }
+   
   // Allocate JsonBuffer
-   const size_t bufferSize = JSON_OBJECT_SIZE(7) + 150;
-   DynamicJsonBuffer jsonBuffer(bufferSize);
+   const size_t capacity = JSON_OBJECT_SIZE(7) + 252;
+   DynamicJsonBuffer jsonBuffer(capacity);
     
     // Parse JSON object
-    JsonObject& object = jsonBuffer.parseObject(line);
-    if (!object.success()) {
+    JsonObject& root = jsonBuffer.parseObject(client);
+    if (!root.success()) {
         Serial.println(F("Parsing failed!"));
         scrollText("JSON Parse Failed!");
     }
-    return object;
+    
+    // Disconnet
+    client.stop();
+    jsonBuffer.clear();
+    return root;
 }
 
 String cryptoURL [] {
@@ -156,7 +147,7 @@ String cryptoURL [] {
 void getCryptoPrices() {
 
       for (int index = 0; index < 5; index++) {
-        char *currentCrypto;
+        String currentCrypto;
         switch (index) {
             case 0:
                 currentCrypto = "BTC $";
@@ -177,37 +168,30 @@ void getCryptoPrices() {
                 break;
         }
         JsonObject& root = getObject(cryptoURL[index]);
-        Serial.println("reply was:");
         Serial.println("==========");
         Serial.println(F("Response:"));
         Serial.print("Trade Id: ");
         Serial.println(root["trade_id"].as<char*>());
         Serial.print("Price: ");
         Serial.println(root["price"].as<char*>());
-        Serial.println("==========");
-        Serial.println("closing connection");
-        
         float cryptoPrice = root["price"].as<float>();
         Serial.println(cryptoPrice);
-        char cryptoArray[64];
-        
-        snprintf(cryptoArray, sizeof cryptoArray, "%.2f", cryptoPrice);
-        char *str2 = cryptoArray;
-        strcat (currentCrypto, str2);
-        Serial.println(currentCrypto);
-        scrollText(currentCrypto);
-        
+        Serial.println("==========");
+       String output = currentCrypto + String(cryptoPrice);
+       Serial.println(output);
+       
+       char *cstr = new char[output.length() + 1];
+       strcpy(cstr, output.c_str());
+       scrollText(cstr);
+       delete [] cstr; 
     }
 }
 
 
 void setup() {
   mx.begin();
-#if  DEBUG
-  Serial.begin(115200);
+  Serial.begin(9600);
   connectToWIFI();
-#endif
-  PRINTS("\n[MD_MAX72XX Test & Demo]");
 }
 
 void loop() {
